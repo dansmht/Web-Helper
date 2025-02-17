41558b05e5ef369a9110ecc171df529aa775be0b
dansmht <dansmht16@gmail.com>
2025-02-18 00:03:37 +0300

    Add CustomIcon to DropdownButtonContent

--- src/components/ThemeDropdown/DropdownButtonContent.tsx
+++ src/components/ThemeDropdown/DropdownButtonContent.tsx
@@ -3,24 +3,27 @@ import { useSystemTheme } from '../../hooks/theme/useSystemTheme.ts';
 
 import LightIcon from '../../assets/icons/light.svg?react';
 import DarkIcon from '../../assets/icons/dark.svg?react';
+import CustomIcon from '../../assets/icons/custom.svg?react';
+
+import type { Theme } from '../../types/themeTypes.ts';
+
+const renderContent = (theme: Theme) => {
+  switch (theme) {
+    case 'light':
+      return <LightIcon />;
+    case 'dark':
+      return <DarkIcon />;
+    default:
+      return <CustomIcon />;
+  }
+};
 
 export const DropdownButtonContent = () => {
   const { theme } = useTheme();
   const systemTheme = useSystemTheme();
 
   const currentTheme = theme === 'system' ? systemTheme : theme;
-  const wrapperClassName = theme !== 'system' ? 'group active' : '';
+  const wrapperClassName = theme === 'system' ? '' : 'group active';
 
-  const renderContent = () => {
-    switch (currentTheme) {
-      case 'light':
-        return <LightIcon />;
-      case 'dark':
-        return <DarkIcon />;
-      default:
-        return 'Custom'; // TODO
-    }
-  };
-
-  return <div className={wrapperClassName}>{renderContent()}</div>;
+  return <div className={wrapperClassName}>{renderContent(currentTheme)}</div>;
 };
