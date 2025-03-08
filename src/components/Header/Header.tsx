9004cd4c4b939c5dda8189016f9bba048c239383
dansmht <dansmht16@gmail.com>
2025-03-08 16:29:14 +0300

    Add home link to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router';
+
 import { LanguageDropdown } from '../LanguageDropdown/LanguageDropdown.tsx';
 import { ThemeDropdown } from '../ThemeDropdown/ThemeDropdown.tsx';
-// TODO Add link to main page
+
 export const Header = () => {
   return (
     <div className="mb-4 flex items-center justify-between px-2 py-6 lg:mb-8">
-      <h1 className="text-2xl font-semibold select-none">Web Helper</h1>
+      <Link to="/">
+        <h1 className="text-2xl font-semibold select-none">Web Helper</h1>
+      </Link>
+
       <div className="flex items-center gap-8">
         <LanguageDropdown />
         <ThemeDropdown />
