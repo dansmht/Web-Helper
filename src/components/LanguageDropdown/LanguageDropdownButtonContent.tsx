d0912517023d3affea6bc637128a08d925461e45
dansmht <dansmht16@gmail.com>
2025-03-09 22:41:42 +0300

    Replace language icons with country flag icons

--- src/components/LanguageDropdown/LanguageDropdownButtonContent.tsx
+++ src/components/LanguageDropdown/LanguageDropdownButtonContent.tsx
@@ -1,10 +1,10 @@
 import { useTranslation } from '../../context/i18n/I18nContext.tsx';
 
-import EnIcon from '../../assets/icons/language/en.svg?react';
-import RuIcon from '../../assets/icons/language/ru.svg?react';
+import EnFlagIcon from '../../assets/icons/language/great-britain-flag.svg?react';
+import RuFlagIcon from '../../assets/icons/language/russia-flag.svg?react';
 
 export const LanguageDropdownButtonContent = () => {
   const { language } = useTranslation();
 
-  return <div>{language === 'en' ? <EnIcon /> : <RuIcon />}</div>;
+  return <div>{language === 'en' ? <EnFlagIcon /> : <RuFlagIcon />}</div>;
 };
--- src/constants/language/languageDropdownOptions.tsx
+++ src/constants/language/languageDropdownOptions.tsx
@@ -1,14 +1,14 @@
 import { computeDropdownOptions } from '../../utils/dropdownUtils.tsx';
 
-import EnIcon from '../../assets/icons/language/en.svg?react';
-import RuIcon from '../../assets/icons/language/ru.svg?react';
+import EnFlagIcon from '../../assets/icons/language/great-britain-flag.svg?react';
+import RuFlagIcon from '../../assets/icons/language/russia-flag.svg?react';
 
 import type { DropdownOptionType } from '../../types/dropdownTypes.ts';
 import type { Language } from '../../types/i18n.ts';
 
 const languageOptions: DropdownOptionType<Language>[] = [
-  { value: 'en', label: 'language.en', icon: <EnIcon /> },
-  { value: 'ru', label: 'language.ru', icon: <RuIcon /> },
+  { value: 'en', label: 'language.en', icon: <EnFlagIcon /> },
+  { value: 'ru', label: 'language.ru', icon: <RuFlagIcon /> },
 ];
 
 export const languageDropdownOptions = computeDropdownOptions(languageOptions);
