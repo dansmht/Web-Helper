d63e1246bc1e6d18e01c5289f5fed693569e58c9
dansmht <dansmht16@gmail.com>
2025-03-23 03:45:27 +0300

    Add Jest coverage configuration

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,26 @@ const config: Config = {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.app.json' }],
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/index.{ts,tsx}',
+    '!src/**/App.tsx',
+    '!src/**/main.tsx',
+    '!src/routing/*.{ts,tsx}',
+    '!src/constants/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+
   moduleNameMapper: {
     '^react-markdown$': '<rootDir>/src/__mocks__/ReactMarkdown.tsx',
     '^remark-gfm$': '<rootDir>/src/__mocks__/remarkGfm.ts',
