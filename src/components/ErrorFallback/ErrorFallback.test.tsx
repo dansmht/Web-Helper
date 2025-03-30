f8ba64e752ccc5e898c66326006551013ad1b218
dansmht <dansmht16@gmail.com>
2025-03-30 05:31:29 +0300

    Refactor ErrorFallback test

--- src/components/ErrorFallback/ErrorFallback.test.tsx
+++ src/components/ErrorFallback/ErrorFallback.test.tsx
@@ -1,32 +1,27 @@
 import { render, screen, fireEvent } from '../../test/testUtils.tsx';
 import { ErrorFallback } from './ErrorFallback';
-import { useTranslation } from '../../context/i18n/I18nContext';
 
 jest.mock('../../context/i18n/I18nContext', () => ({
   ...(jest.requireActual(
     '../../context/i18n/I18nContext'
   ) as unknown as object),
-  useTranslation: jest.fn(),
+  useTranslation: () => ({
+    t: (key: string) => {
+      const translations: Record<string, string> = {
+        'error.something_went_wrong': 'Something went wrong',
+        try_again: 'Try Again',
+      };
+      return translations[key] || key;
+    },
+  }),
 }));
 
 describe('ErrorFallback', () => {
-  const mockUseTranslation = useTranslation as jest.Mock;
-
-  const mockTranslation = (key: string) => {
-    const translations: Record<string, string> = {
-      'error.something_went_wrong': 'Something went wrong',
-      try_again: 'Try Again',
-    };
-    return translations[key] || key;
-  };
-
   const error = new Error('Test error message');
   const mockResetErrorBoundary = jest.fn();
 
   beforeEach(() => {
     jest.resetAllMocks();
-
-    mockUseTranslation.mockReturnValue({ t: mockTranslation });
   });
 
   it('renders the error message correctly', () => {
